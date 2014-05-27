42b41e4e23d34cbc5fa0f7abcc253f7567ff5772
iMinder <captain_jackie@foxmail.com>
2014-05-27 16:33:52 +0800

    实现了相机的简单功能

--- Papercut/ShowCameraViewController.h
+++ Papercut/ShowCameraViewController.h
@@ -11,6 +11,4 @@
 
 @interface ShowCameraViewController : UIViewController
 
-@property (strong, nonatomic) GPUImageVideoCamera *videoCamera;
-
 @end
--- Papercut/ShowCameraViewController.m
+++ Papercut/ShowCameraViewController.m
@@ -8,14 +8,41 @@
 
 #import "ShowCameraViewController.h"
 
+@interface ShowCameraViewController()
+
+@property (strong, nonatomic)GPUImageVideoCamera *videoCamera;
+@property (strong, nonatomic)GPUImageView *cameraImageView;
+@property (strong, nonatomic)GPUImageFilter *filter;
+@end
+
 @implementation ShowCameraViewController
+
+- (IBAction)dismissModalView:(id)sender
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
-    _videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+}
+
+- (void)awakeFromNib
+{
+    self.cameraImageView = [[GPUImageView alloc]initWithFrame:CGRectMake(0, 64, 320, 380)];
+    self.videoCamera = [[GPUImageVideoCamera alloc]initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
+    self.videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    [self.view addSubview:self.cameraImageView];
+    
+    self.filter = [[GPUImageFilter alloc]initWithFragmentShaderFromFile:@"Shader1"];
+    [_filter forceProcessingAtSize:self.cameraImageView.sizeInPixels];
+   
+    [self.videoCamera addTarget:_filter];
+    [_filter addTarget:self.cameraImageView];
+    
+    [self.videoCamera startCameraCapture];
     
 }
+
 @end
